786a10479c8cf4028bbbd129607ead892129c46f
onigbindeseye <seyeonigbinde87@gmail.com>
2022-10-06 00:05:24 +0100

    Initial Setup of React App

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 }
+    ],
+    'global-require': 0,
+    'linebreak-style': [0,
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ]
+  }
+};

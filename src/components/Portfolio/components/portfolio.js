66dd01d2c14702a8e09efc8b37b6e35cec00321a
onigbindeseye <seyeonigbinde87@gmail.com>
2023-01-01 16:15:15 +0100

    Additional Projects added to Portfolio

--- src/components/Portfolio/components/portfolio.js
+++ src/components/Portfolio/components/portfolio.js
@@ -6,6 +6,8 @@ import linker from 'images/linker.png';
 import motivator from 'images/motivator.png';
 import budgit from 'images/budgit.png';
 import covid from 'images/covid.png';
+import calculator from 'images/calculator.png';
+import lovingsitter from 'images/lovingsitter.png';
 
 export const portfolio = [
   {
@@ -19,7 +21,7 @@ export const portfolio = [
     stack3: 'Nodejs',
     stack4: 'Express',
     stack5: 'PostgreSQL',
-    github: 'http://github.com/',
+    github: 'http://github.com/onigbindeseye',
     web: 'https://grap.com/',
   },
   {
@@ -33,7 +35,7 @@ export const portfolio = [
     stack3: 'Nodejs',
     stack4: 'Express',
     stack5: 'PostgreSQL',
-    github: 'http://github.com/',
+    github: 'http://github.com/onigbindeseye',
     web: 'https://yvotenaija.org',
   },
   {
@@ -47,7 +49,7 @@ export const portfolio = [
     stack3: 'Nodejs',
     stack4: 'Express',
     stack5: 'PostgreSQL',
-    github: 'http://github.com/',
+    github: 'http://github.com/onigbindeseye',
     web: 'https://loginaija.com',
   },
   {
@@ -61,7 +63,7 @@ export const portfolio = [
     stack3: 'Nodejs',
     stack4: 'Express',
     stack5: 'PostgreSQL',
-    github: 'http://github.com/',
+    github: 'http://github.com/onigbindeseye',
     web: 'https://frontend-policy-shapers.vercel.app/',
   },
   {
@@ -75,7 +77,7 @@ export const portfolio = [
     stack3: 'Nodejs',
     stack4: 'Express',
     stack5: 'PostgreSQL',
-    github: 'http://github.com/',
+    github: 'http://github.com/onigbindeseye',
     web: 'https://linker.ng',
   },
   {
@@ -89,7 +91,7 @@ export const portfolio = [
     stack3: 'Nodejs',
     stack4: 'Express',
     stack5: 'PostgreSQL',
-    github: 'http://github.com/',
+    github: 'http://github.com/onigbindeseye',
     web: 'https://motivators-crib-africa.herokuapp.com/',
   },
   {
@@ -97,10 +99,10 @@ export const portfolio = [
     imgpath: budgit,
     title: 'Active Citizen Award',
     description:
-      'Active Citizen Award is BudgIT’s initiative and signature move to spotlight and appreciate personalities, organisations and agencies doing these ground-breaking humanitarian work in Nigeria. They believe that the ACA will stir more conversations on the need to recognise the tremendous contributions of our civic leaders to effective governance and an active civic space in Nigeria.',
+      'Active Citizen Award is BudgIT’s initiative and signature move to spotlight and appreciate personalities, organizations and agencies doing these ground-breaking humanitarian work in Nigeria. They believe that the ACA will stir more conversations on the need to recognize the tremendous contributions of our civic leaders to effective governance and an active civic space in Nigeria.',
     stack1: 'React',
     stack2: 'Material-UI',
-    github: 'http://github.com/',
+    github: 'http://github.com/onigbindeseye',
     web: 'https://awards.civichive.org/',
   },
   {
@@ -111,7 +113,47 @@ export const portfolio = [
       'This project is executed by Oxlade Consulting in collaboration with LEGS Senegal, BudgIT Ghana and Seed4Africa. Our project is focused on Advancing Community Access to Equitable COVID-19 Response (CACER) in three West African countries - Nigeria, Senegal and Ghana. They have chosen three countries within the anglophone and francophone sphere which are the main economies of West Africa and are also locations of research conducted by Dalberg.',
     stack1: 'React',
     stack2: 'Material-UI',
-    github: 'http://github.com/',
+    github: 'http://github.com/onigbindeseye',
     web: 'https://cacer.africa/',
+  },
+  {
+    id: 9,
+    imgpath: calculator,
+    title: 'Calculator',
+    description:
+      'A Simple React Single-Digit Calculator Created Using Reducer Patterns.',
+    stack1: 'React',
+    stack2: 'Redux',
+    stack3: 'Javascript',
+    stack4: 'Bootstrap',
+    github: 'https://github.com/seyeonigbinde/Calculator-App',
+    web: 'https://calculator-app-psi.vercel.app/',
+  },
+  {
+    id: 10,
+    imgpath: lovingsitter,
+    title: 'LovingSitter',
+    description:
+      'LovingSitter is an ongoing web application that makes it easier for dog owners to find a dog sitter especially in real-time. The goal is to create a beautiful, easy-to-use website that helps dog owners find dog sitters when they need it most. We want dog owners to feel secure, comfortable and at ease with their decision to book a dog sitter. We want dog sitters to feel excited to meet and care for a new dog.  This project was created by Hatchways Career Accelerator Program and I collaborated with two other developers on this.',
+    stack1: 'React',
+    stack2: 'Typescript',
+    stack3: 'MongoDB',
+    stack4: 'Material-UI',
+    stack5: 'Node',
+    github: 'https://github.com/hatchways/team-bruschetta',
+    web: '',
+  },
+  {
+    id: 11,
+    imgpath: lovingsitter,
+    title: 'Vicky Stores',
+    description:
+      'Vicky Stores is an e-commerce site using FakeStoreAPI. It allows you to select products from various categories and checkout easily.',
+    stack1: 'React',
+    stack2: 'Material-UI',
+    stack3: 'Javascript',
+    stack4: 'FakeStoreAPI',
+    github: 'https://github.com/seyeonigbinde/vicky-stores',
+    web: 'https://vicky-stores.vercel.app/',
   },
 ];

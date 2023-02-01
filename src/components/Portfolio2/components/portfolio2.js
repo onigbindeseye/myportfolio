a2367f4e687f8b534016422eef127a9336f62e6a
onigbindeseye <seyeonigbinde87@gmail.com>
2023-02-01 15:01:12 +0100

    Page 2 of Portfolio Component Created

---
+++ src/components/Portfolio2/components/portfolio2.js
@@ -0,0 +1,93 @@
+import grap from 'images/grap.png';
+import yvotenaija from 'images/yvotenaija.png';
+import login from 'images/login.png';
+import policy from 'images/policy.png';
+import linker from 'images/linker.png';
+import motivator from 'images/motivator.png';
+
+export const portfolio2 = [
+  {
+    id: 1,
+    imgpath: grap,
+    title: 'Grassroot Advocacy Project - GRAP',
+    description:
+      'GRAP is a civic-tech initiative for budget participation by PROMAD—using tech to document the NEEDS of Nigerian communities for adoption into the annual budget. The application is designed to fill the gap and to engage communities and citizens digitally and offline to conduct NEEDS assessment which would be adopted in the budget for the people and empower them on how to hold the government accountable.',
+    stack1: 'React',
+    stack2: 'Material-UI',
+    stack3: 'Nodejs',
+    stack4: 'Express',
+    stack5: 'PostgreSQL',
+    github: 'http://github.com/onigbindeseye',
+    web: 'https://grap.org.ng/',
+  },
+  {
+    id: 2,
+    imgpath: yvotenaija,
+    title: 'YVoteNaija ',
+    description:
+      'YVoteNaija is an initiative of Brain Builders Youth development Initiative which aims at increasing voter education through data driven engagement, orientating young Nigerians on the importance of getting involved in the process especially at the grassroot level.',
+    stack1: 'React',
+    stack2: 'Material-UI',
+    stack3: 'Nodejs',
+    stack4: 'Express',
+    stack5: 'PostgreSQL',
+    github: 'http://github.com/onigbindeseye',
+    web: 'https://yvotenaija.org',
+  },
+  {
+    id: 3,
+    imgpath: login,
+    title: 'Local Open Governance Initiative in Nigeria - LOGIN ',
+    description:
+      'LOGIN is an emerging civic hub aimed at launching strategic cum calculated design-thinking civic action melded with tech premised on Open Governance System for tracking/monitoring projects, budgets, expenditures & financial flows including Federal Account Allocation Committee (FAAC) monthly releases as well as elections; by citizens & stakeholders for the purpose of promoting good governance, accountability, and transparency at the third tier of government in Nigeria.',
+    stack1: 'React',
+    stack2: 'Material-UI',
+    stack3: 'Nodejs',
+    stack4: 'Express',
+    stack5: 'PostgreSQL',
+    github: 'http://github.com/onigbindeseye',
+    web: 'http://loginaija.com',
+  },
+  {
+    id: 4,
+    imgpath: policy,
+    title: 'Policy Shapers',
+    description:
+      'Policy Shapers is a youth-led open source platform for policy ideas that address the world’s most pressing challenges. The platform features policy ideas worth implementing. Through Policy Shapers they hope to inspire the next generation of policymakers and provide them a platform to share their ideas and collaborate for change. The policy ideas shared on this platform are accessible publicly for anyone to learn from and this is in line with core open source principles of open exchange, collaborative participation, transparency, meritocracy, and community-oriented development.',
+    stack1: 'React',
+    stack2: 'Material-UI',
+    stack3: 'Nodejs',
+    stack4: 'Express',
+    stack5: 'PostgreSQL',
+    github: 'http://github.com/onigbindeseye',
+    web: 'https://frontend-policy-shapers.vercel.app/',
+  },
+  {
+    id: 5,
+    imgpath: linker,
+    title: 'Linker',
+    description:
+      'Linker is a civic-tech idea of Urban Alert, designed to build a society where all spheres of governance work for all citizens irrespective of their social status, literacy level, disability or geographical location. They do this by reawakening citizens to engage established ministries, departments and agencies of government on social problems they are facing. Linker helps citizens to find the ministries, departments and agencies of government established to solve their problems and track the response rate of these public offices for institutional accountability and policy reform',
+    stack1: 'React',
+    stack2: 'Material-UI',
+    stack3: 'Nodejs',
+    stack4: 'Express',
+    stack5: 'PostgreSQL',
+    github: 'http://github.com/onigbindeseye',
+    web: 'http://linker.ng',
+  },
+  {
+    id: 6,
+    imgpath: motivator,
+    title: 'Motivator’s Crib Africa',
+    description:
+      'Motivator’s Crib Africa is interested in promoting good health and Mental well-being through drug abuse prevention and treatment. It intends to use technology and data to build an effective and sustainable framework for advocacy on drug abuse prevention. And also create an intervention support system in response to stigma free, harm reduction, availability, accessibility and affordability of treatment and recovery services (continuum of care) for People Who Use Drugs (PWUDs), Women who use/inject drugs (WWU/IDs), high risk drug users and Most-at-risk and vulnerable Populations (MAR&VPs).',
+    stack1: 'React',
+    stack2: 'Material-UI',
+    stack3: 'Nodejs',
+    stack4: 'Express',
+    stack5: 'PostgreSQL',
+    github: 'http://github.com/onigbindeseye',
+    web: 'https://motivators-crib-africa.herokuapp.com/',
+  },
+];

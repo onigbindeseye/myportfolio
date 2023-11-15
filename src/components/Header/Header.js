04fe33280790eabbc8d65747c6b15ac8327757d1
onigbindeseye <seyeonigbinde87@gmail.com>
2023-11-15 13:59:52 +0100

    Mobile Header Component Updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,24 +4,66 @@ import { useTheme, styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Fade from '@mui/material/Fade';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from '@mui/material';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import CloseIcon from '@mui/icons-material/Close';
+import Divider from '@mui/material/Divider';
+import navConfig from './config';
 import logo from 'images/logo.png';
 
 const Header = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [state, setState] = React.useState({
+    right: false,
+  });
 
-  const open = Boolean(anchorEl);
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleDrawerClose = () => {
+    setState(false);
   };
-  const handleClose = () => {
-    setAnchorEl(null);
+
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (
+      event &&
+      event.type === 'keydown' &&
+      (event.key === 'Tab' || event.key === 'Shift')
+    ) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
   };
+
+  const list = (anchor) => (
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {navConfig.map((text, index) => (
+          <ListItem key={index} disablePadding>
+            <ListItemButton href={text.path}>
+              <ListItemText primary={text.title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  }));
 
   const ColorButton = styled(Button)(() => ({
     color: '#43D8C9',
@@ -87,7 +129,7 @@ const Header = () => {
             <Link
               underline="none"
               component="a"
-              href="/portfolio/page1"
+              href="/portfolio"
               color="#43D8C9"
             >
               Portfolio
@@ -111,72 +153,63 @@ const Header = () => {
           </Box>
         </Box>
         <Box sx={{ display: { xs: 'flex', md: 'none' } }} width={'100%'}>
-          <AppBar
-            position="fixed"
-            zIndex={theme.zIndex.drawer + 1}
-            sx={{
-              backgroundColor: '#0D3446',
-            }}
-          >
-            <Toolbar>
-              <Box
-                component="a"
-                underline="none"
-                href="/"
-                title="Oluseye Onigbinde"
-                width={90}
-                marginRight={25}
-                marginTop={2}
-                marginBottom={1}
-              >
+          <div>
+            <AppBar
+              position="fixed"
+              zIndex={theme.zIndex.drawer + 1}
+              sx={{
+                backgroundColor: '#0D3446',
+              }}
+            >
+              <Toolbar>
                 <Box
-                  component="img"
-                  height={'5vh'}
-                  sx={{
-                    display: 'block',
-                    position: 'relative',
-                    overflow: 'hidden',
-                    width: '100%',
-                  }}
-                  src={logo}
-                  alt={'logo'}
-                />
-              </Box>
-              <IconButton
-                aria-label="more"
-                id="composition-button"
-                aria-controls={open ? 'composition-menu' : undefined}
-                aria-expanded={open ? 'true' : undefined}
-                aria-haspopup="true"
-                onClick={handleClick}
-              >
-                <MenuIcon fontSize="large" sx={{ color: '#43D8C9 ' }} />
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-          <Menu
-            id="fade-menu"
-            MenuListProps={{
-              'aria-labelledby': 'fade-button',
-            }}
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            TransitionComponent={Fade}
-          >
-            <MenuItem onClick={handleClose} component="a" href="/">
-              Home
-            </MenuItem>
-            <MenuItem onClick={handleClose} component="a" href="/profile">
-              Profile
-            </MenuItem>
-            <MenuItem onClick={handleClose} component="a" href="/portfolio/page1">
-              Portfolio
-            </MenuItem>
-            <MenuItem onClick={handleClose} component="a" href="/blog">
-              Blog
-            </MenuItem>
-          </Menu>
+                  component="a"
+                  underline="none"
+                  href="/"
+                  title="Oluseye Onigbinde"
+                  width={'100%'}
+                  marginRight={29}
+                  marginTop={2}
+                  marginBottom={2}
+                >
+                  <Box
+                    component="img"
+                    height={'4vh'}
+                    sx={{
+                      display: 'block',
+                      position: 'relative',
+                      overflow: 'hidden',
+                      width: '100%',
+                    }}
+                    src={logo}
+                    alt={'logo'}
+                  />
+                </Box>
+                {['right'].map((anchor) => (
+                  <React.Fragment key={anchor}>
+                    <IconButton onClick={toggleDrawer(anchor, true)}>
+                      <MenuIcon fontSize="large" sx={{ color: '#43D8C9' }} />
+                    </IconButton>
+                    <SwipeableDrawer
+                      anchor={anchor}
+                      open={state[anchor]}
+                      onClose={toggleDrawer(anchor, false)}
+                      onOpen={toggleDrawer(anchor, true)}
+                    >
+                      <DrawerHeader>
+                        <IconButton onClick={handleDrawerClose}>
+                          <CloseIcon sx={{ color: '#43D8C9' }} />
+                        </IconButton>
+                      </DrawerHeader>
+                      <Divider />
+                      {list(anchor)}
+                      <Divider />
+                    </SwipeableDrawer>
+                  </React.Fragment>
+                ))}
+              </Toolbar>
+            </AppBar>
+          </div>
         </Box>
       </Box>
     </Box>
---
+++ src/components/Header/config.js
@@ -0,0 +1,20 @@
+const navConfig = [
+  {
+    title: 'Home',
+    path: '/',
+  },
+  {
+    title: 'Profile',
+    path: '/profile',
+  },
+  {
+    title: 'Portfolio',
+    path: '/portfolio',
+  },
+  {
+    title: 'Blog',
+    path: '/blog',
+  },
+];
+
+export default navConfig;
